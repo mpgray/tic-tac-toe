77a2ae3f9c0848f1cde7f4797e2db4e58c1ebbcd
Michael <michael@mgray.me>
2020-12-02 23:07:11 -0700

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { StatsComponent } from './stats/stats.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    StatsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
